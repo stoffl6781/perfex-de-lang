965c25f042fd4508af4c5e386ce4daf05122462d
Christoph Purin <christoph@purin.at>
2022-05-13 11:20:42 +0200

    QR Code Lang Update

---
+++ boxvibe/qrcode/language/german/qrcode_lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+language Name: German
+Description: German Language to QRCode Module for Perfex CRM
+Author:  Christoph Purin
+Author URI: purin.at
+Version: 1.0.0
+Module Version: 1.0.0
+Update: 13.05.2022
+*/
+
+$lang["qrcode"] = "QR Code";
+$lang["qrcode_credit_note_qr_code_info"] = "QR Code Inhalt";
+$lang["qrcode_estimate_qr_code_info"] = "QR Code Inhalt";
+$lang["qrcode_invoice_qr_code_info"] = "QR Code Inhalt";
+$lang["qrcode_payment_qr_code_info"] = "QR Code Inhalt";
+$lang["qrcode_proposal_qr_code_info"] = "Code Information";
+$lang["qrcode_show_credit_note_qr_code"] = "Gutschriften";
+$lang["qrcode_show_estimate_qr_code"] = "Voranschlag";
+$lang["qrcode_show_invoice_qr_code"] = "Rechnung";
+$lang["qrcode_show_payment_qr_code"] = "Zahlung";
+$lang["qrcode_show_proposal_qr_code"] = "Kostenvoranschlag";
+$lang["qr_code_height"] = "QR Code Höhe";
+$lang["qr_code_width"] = "QR Code Breite";
+$lang["qr_code_x_position"] = "X - Position";
+$lang["qr_code_y_position"] = "Y Position";
